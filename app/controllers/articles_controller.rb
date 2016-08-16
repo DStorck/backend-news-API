8a14743d81564b4717a50cb3c4dfc4fd25d2a89e
Deirdre Storck <deirdre.storck@gmail.com>
2016-08-16 14:40:03 -0700

    fixed a typo

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,7 @@ class ArticlesController < ApplicationController
   def by_topic_page
     @articles = GuardianAPIWrapper.section_by_page(params[:topic], params[:page])
     render json: @articles
+  end
 
   def welcome
     render json: "Welcome to the backend."
