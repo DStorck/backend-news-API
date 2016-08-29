cf80a2b2ea70a6da914a4fcf11421bad2b6944cc
Deirdre Storck <deirdre.storck@gmail.com>
2016-08-29 13:13:25 -0700

    added MIT license to repo

--- lib/guardian_api_wrapper.rb
+++ lib/guardian_api_wrapper.rb
@@ -6,67 +6,23 @@ BASE_URL = "http://content.guardianapis.com/search?order-by=newest&api-key=#{API
 
 
   def self.section(section)
-    @articles = []
     initial_response = HTTParty.get(BASE_URL + "&section=" + section)
     return initial_response
   end
 
   def self.section_by_page(section, page)
-    @articles = []
     initial_response = HTTParty.get(BASE_URL + "&section=" + section + "&page=" + page )
     return initial_response
   end
 
   def self.search(keyword)
-    @articles = []
     initial_response = HTTParty.get(BASE_URL + "&q=" + keyword)
     return initial_response
   end
 
   def self.search_by_page(keyword, page)
-    @articles = []
     initial_response = HTTParty.get(BASE_URL + "&q=" + keyword + "&page=" + page)
     return initial_response
   end
 
 end
-
-
-
-# def self.section(section)
-#   @articles = []
-#   initial_response = HTTParty.get(BASE_URL + "&section=" + section)
-#
-#   initial_response['response']['results'].each do |article|
-#     @articles.push Article.create_article(article)
-#   end
-#   @articles
-# end
-
-# def self.section_by_page(section, page)
-#   @articles = []
-#   initial_response = HTTParty.get(BASE_URL + "&section=" + section + "&page=" + page )
-#   initial_response['response']['results'].each do |article|
-#     @articles.push Article.create_article(article)
-#   end
-#   @articles
-# end
-
-# def self.search(keyword)
-#   @articles = []
-#   initial_response = HTTParty.get(BASE_URL + "&q=" + keyword)
-#   initial_response['response']['results'].each do |article|
-#     @articles.push Article.create_article(article)
-#   end
-#   @articles
-# end
-
-#
-# def self.search_by_page(keyword, page)
-#   @articles = []
-#   initial_response = HTTParty.get(BASE_URL + "&q=" + keyword + "&page=" + page)
-#   initial_response['response']['results'].each do |article|
-#     @articles.push Article.create_article(article)
-#   end
-#   @articles
-# end
