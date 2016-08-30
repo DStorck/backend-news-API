17bf5d9cb43f3d824ab53ff48ab2a57adce20e56
Deirdre Storck <deirdre.storck@gmail.com>
2016-08-30 14:03:01 -0700

    fixed search method in api wrapper

--- lib/guardian_api_wrapper.rb
+++ lib/guardian_api_wrapper.rb
@@ -3,6 +3,7 @@ require "httparty"
 module GuardianAPIWrapper
 API_KEY = ENV["GUARDIAN_API_KEY"]
 BASE_URL = "http://content.guardianapis.com/search?order-by=newest&api-key=#{API_KEY}"
+SEARCH_BASE = "https://content.guardianapis.com/search?&api-key=#{API_KEY}&q="
 
 
   def self.section(section)
@@ -16,12 +17,12 @@ BASE_URL = "http://content.guardianapis.com/search?order-by=newest&api-key=#{API
   end
 
   def self.search(keyword)
-    initial_response = HTTParty.get(BASE_URL + "&q=" + keyword)
+    initial_response = HTTParty.get(SEARCH_BASE + keyword)
     return initial_response
   end
 
   def self.search_by_page(keyword, page)
-    initial_response = HTTParty.get(BASE_URL + "&q=" + keyword + "&page=" + page)
+    initial_response = HTTParty.get(SEARCH_BASE + keyword + "&page=" + page)
     return initial_response
   end
 
